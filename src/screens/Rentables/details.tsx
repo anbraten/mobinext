013d92be2b2428012a59dbdd3bc875b6a8f3df00
programonaut <dev@maximilian-kuerschner.de>
2022-11-24 17:40:00 +0100

    feat: add rentables to database

--- src/screens/Rentables/details.tsx
+++ src/screens/Rentables/details.tsx
@@ -7,15 +7,68 @@ import {
     Card,
     Button,
     SegmentedButtons,
+    TextInput
   } from "react-native-paper";
+import { supabase } from "~/supabase";
+import { Rentable } from "~/types";
 
 export const Details = ({ navigation }: any) => {
+    const [rentable, setRentable] = React.useState<Rentable>({} as Rentable);
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Details 2</Text>
-        <Button onPress={() => navigation.navigate("Discover")} mode="contained">
-          To Discover
+      <View style={{ margin: "5%" }}>
+        <View style={{width: "100%", marginBottom: 10}}>
+          <Text variant="titleMedium">Modell:</Text>
+          <TextInput
+            onChangeText={rentable.model}
+            value={rentable.model}
+          ></TextInput>
+        </View>
+
+        <View style={{width: "100%", marginBottom: 10 }}>
+          <Text variant="titleMedium">Sitze:</Text>
+          <TextInput></TextInput>
+        </View>
+
+        <View style={{width: "100%", marginBottom: 10 }}>
+          <Text variant="titleMedium">Treibstoff:</Text>
+          <TextInput
+            onChangeText={rentable.fuel}
+            value={rentable.fuel}
+          ></TextInput>
+        </View>
+
+        <View style={{width: "100%", marginBottom: 10 }}>
+          <Text variant="titleMedium">Fahrzeug Standort:</Text>
+          <TextInput
+            onChangeText={rentable.city}
+            value={rentable.city}
+          ></TextInput>
+        </View>
+
+        <View style={{width: "100%", marginBottom: 10 }}>
+          <Text variant="titleMedium">Weitere Informationen:</Text>
+          <TextInput 
+            multiline
+            numberOfLines={5}
+            onChangeText={rentable.city}
+            value={rentable.city}
+          ></TextInput>
+        </View>
+        
+        <Button onPress={() => createCar(rentable)} mode="contained">
+          Create Rentable
         </Button>
       </View>
     );
   };
+
+async function createCar(rentable: Rentable) {
+  const { data, error, status } = await supabase
+  .from('rentables')
+  .insert([
+    rentable
+  ])
+
+  console.log(data, error, status);
+  
+}
