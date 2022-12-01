b71a0bbe98f302c28a2415a37f9de00da8c53787
Anbraten <anton@ju60.de>
2022-12-01 16:44:50 +0100

    add push notifications listener

--- App.tsx
+++ App.tsx
@@ -2,13 +2,16 @@ import { Provider as PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AuthProvider } from "./src/provider/AuthProvider";
 import Navigation from "./src/navigation";
+import { NotificationProvider } from "~/provider/NotificationProvider";
 
 const App = () => {
   return (
     <PaperProvider>
       <SafeAreaProvider>
         <AuthProvider>
-          <Navigation />
+          <NotificationProvider>
+            <Navigation />
+          </NotificationProvider>
         </AuthProvider>
       </SafeAreaProvider>
     </PaperProvider>
