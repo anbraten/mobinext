55fbb86023373b44576abee2b77197d8597815b7
ekincioglu <ekincioglu@protonmail.com>
2022-12-31 14:50:09 +0100

    Added placeholder content for giveback screen

--- src/screens/GiveBackRentedVehicle/index.tsx
+++ src/screens/GiveBackRentedVehicle/index.tsx
@@ -1,5 +1,8 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useEffect, useState } from "react";
+import { ScrollView, View, Text } from "react-native";
+import { Card, Button } from "react-native-paper";
+import CardContent from "react-native-paper/lib/typescript/components/Card/CardContent";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 import { RootStackParamList } from "~/navigation/subnavigation/MainStack";
@@ -14,6 +17,41 @@ export const GiveBackRentedVehicle = ({ route, navigation }: Props) => {
 
   return (
     <SafeAreaView style={{ flex: 1 }} edges={["bottom", "left", "right"]}>
+      <ScrollView style={{ height: "90%" }}>
+        <View style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            padding: 35,
+          }}>
+        <Card>
+          <Card.Content>
+            <View style={{
+              paddingBottom: 50
+            }}>
+              <Text>
+                Möchten du den VW T4 wirklich zurückgeben?
+              </Text>
+              <Text style={{
+                paddingTop: 30
+              }}>
+                Weitere Informationen: bla bla
+              </Text>
+            </View>
+            <Button
+              mode="contained"
+              onPress={async () => {
+                // await giveBackCar();
+                // navigation.navigate("...");
+              }}
+            >
+            Fahrzeug zurückgeben
+            </Button>
+          </Card.Content>
+        </Card>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
