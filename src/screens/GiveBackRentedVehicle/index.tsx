801502f49e36043afeebaf2b01e76e082f13fcdb
ekincioglu <ekincioglu@protonmail.com>
2023-01-02 15:09:23 +0100

    Removes reservation but does not work everytime for some reason

--- src/screens/GiveBackRentedVehicle/index.tsx
+++ src/screens/GiveBackRentedVehicle/index.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { ScrollView, View, Text } from "react-native";
 import { Card, Button } from "react-native-paper";
 import CardContent from "react-native-paper/lib/typescript/components/Card/CardContent";
+import { supabase } from "~/supabase";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 import { RootStackParamList } from "~/navigation/subnavigation/MainStack";
@@ -10,10 +11,35 @@ import { Rentable } from "~/types";
 
 type Props = NativeStackScreenProps<RootStackParamList, "GiveBackRentedVehicle">;
 
-export const GiveBackRentedVehicle = ({ route, navigation }: Props) => {
-    const [selectedRentable, setSelectedRentable] = useState<Rentable>(
-        route.params.selectedRentable
-      );
+export const GiveBackRentedVehicle = ({ route, navigation }: any) => {
+  const [selectedRentable, setSelectedRentable] = useState<Rentable | undefined>(
+    route.params.selectedRentable
+  );
+
+  const currentReservation = route.params.reservation;
+  const vehicleInfo = route.params.reservation._rentable;
+
+  //console.log("Das Fahrzeug " + currentReservation.rentable + ", mit dem Namen " + vehicleInfo.model + " ist im Moment ausgeliehen von " + currentReservation.borrower + ".")
+
+    const removeSupabaseCarEntry = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+  
+      const { data, status, error } = await supabase
+        .from('reservations')
+        .delete()
+        .eq('id', currentReservation.id)
+        // .match({
+        //   rentable: currentReservation.rentable,
+        //   borrower: currentReservation.borrower,
+        // })
+      
+      console.log(error);
+      console.log(data);
+      console.log(status);
+      console.log(currentReservation.id + " should be gone now")
+    };
 
   return (
     <SafeAreaView style={{ flex: 1 }} edges={["bottom", "left", "right"]}>
@@ -31,19 +57,19 @@ export const GiveBackRentedVehicle = ({ route, navigation }: Props) => {
               paddingBottom: 50
             }}>
               <Text>
-                Möchten du den VW T4 wirklich zurückgeben?
+                Möchten du das Fahrzeug "{vehicleInfo.model}" wirklich zurückgeben?
               </Text>
               <Text style={{
                 paddingTop: 30
               }}>
-                Weitere Informationen: bla bla
+                Weitere Informationen: {vehicleInfo.additional_information}
               </Text>
             </View>
             <Button
               mode="contained"
               onPress={async () => {
-                // await giveBackCar();
-                // navigation.navigate("...");
+                await removeSupabaseCarEntry();
+                navigation.navigate("Reservations");
               }}
             >
             Fahrzeug zurückgeben
