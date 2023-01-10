dc3cab241f5466c23a8cf237c09f8a61a9cca50b
ekincioglu <ekincioglu@protonmail.com>
2023-01-10 13:13:53 +0100

    Updated ui + added maps to return screen

--- src/screens/GiveBackRentedVehicle/index.tsx
+++ src/screens/GiveBackRentedVehicle/index.tsx
@@ -1,13 +1,22 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useEffect, useState } from "react";
-import { ScrollView, View, Text } from "react-native";
-import { Card, Button } from "react-native-paper";
+import { ScrollView, View, Platform } from "react-native";
+import { Card } from "react-native-paper";
 import CardContent from "react-native-paper/lib/typescript/components/Card/CardContent";
 import { supabase } from "~/supabase";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import * as Location from "expo-location";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 import { RootStackParamList } from "~/navigation/subnavigation/MainStack";
 import { Rentable } from "~/types";
+import {
+  Button,
+  Divider,
+  IconButton,
+  MD3Colors,
+  Text,
+} from "react-native-paper";
 
 type Props = NativeStackScreenProps<RootStackParamList, "GiveBackRentedVehicle">;
 
@@ -16,10 +25,51 @@ export const GiveBackRentedVehicle = ({ route, navigation }: any) => {
     route.params.selectedRentable
   );
 
+  const isIOS = Platform.OS === "ios";
+  const [show, setShow] = useState(false);
+  const [showErorr, setShowError] = useState(false);
+
+  const [locationAddress, setLocationAddress] =
+    useState<Location.LocationGeocodedAddress[]>();
+  let map: MapView;
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (vehicleInfo.latitude && vehicleInfo.longitude) {
+        map.animateToRegion({
+          latitude: vehicleInfo.latitude,
+          longitude: vehicleInfo.longitude,
+          latitudeDelta: 0.002,
+          longitudeDelta: 0.002,
+        });
+      }
+    }, 500);
+  }, [selectedRentable]);
+
+  useEffect(() => {
+    if (vehicleInfo.latitude && vehicleInfo.longitude) {
+      const coords = {
+        latitude: vehicleInfo.latitude,
+        longitude: vehicleInfo.longitude,
+      };
+      const { latitude, longitude } = coords;
+
+      const getAdress = async () => {
+        const response = await Location.reverseGeocodeAsync({
+          latitude,
+          longitude,
+        });
+        if (response) {
+          setLocationAddress(response);
+        }
+      };
+
+      getAdress();
+    }
+  }, [selectedRentable]);
+
   const currentReservation = route.params.reservation;
   const vehicleInfo = route.params.reservation._rentable;
-
-  //console.log("Das Fahrzeug " + currentReservation.rentable + ", mit dem Namen " + vehicleInfo.model + " ist im Moment ausgeliehen von " + currentReservation.borrower + ".")
 
     const removeSupabaseCarEntry = async () => {
       const {
@@ -30,10 +80,6 @@ export const GiveBackRentedVehicle = ({ route, navigation }: any) => {
         .from('reservations')
         .delete()
         .eq('id', currentReservation.id)
-        // .match({
-        //   rentable: currentReservation.rentable,
-        //   borrower: currentReservation.borrower,
-        // })
       
       console.log(error);
       console.log(data);
@@ -41,43 +87,230 @@ export const GiveBackRentedVehicle = ({ route, navigation }: any) => {
       console.log(currentReservation.id + " should be gone now")
     };
 
-  return (
-    <SafeAreaView style={{ flex: 1 }} edges={["bottom", "left", "right"]}>
-      <ScrollView style={{ height: "90%" }}>
-        <View style={{
-            width: "100%",
+
+    return (<SafeAreaView style={{ flex: 1 }} edges={["bottom", "left", "right"]}>
+    <ScrollView style={{ height: "90%" }}>
+      <View
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          padding: 10,
+        }}
+      >
+        <View>
+          <IconButton
+            mode="outlined"
+            icon="car"
+            iconColor={MD3Colors.neutral50}
+            size={25}
+          />
+        </View>
+        <View
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: 10,
+            marginBottom: 10,
+          }}
+        >
+          <Text variant="titleLarge">{vehicleInfo.model}</Text>
+          <View>
+            <Text variant="titleSmall">
+              Kraftstoff: {vehicleInfo.fuel}
+            </Text>
+            <Text variant="titleSmall">
+              Kosten per km: {vehicleInfo.cost_per_km}€
+            </Text>
+            <Text variant="titleSmall">
+              Kosten per minute: {vehicleInfo.cost_per_minute}€
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: 10,
+            marginBottom: 10,
+          }}
+        >
+          <Text variant="titleLarge">
+            Sitze: {vehicleInfo.seat_count}
+          </Text>
+        </View>
+      </View>
+      <Divider />
+      <View
+        style={{
+          padding: 10,
+        }}
+      >
+        <Text variant="titleLarge">Location</Text>
+        <View
+          style={{
+            height: 125,
+            width: "45%",
+            borderColor: MD3Colors.neutral50,
+            borderStyle: "solid",
+            borderWidth: 1.5,
+            borderRadius: 3,
+            marginTop: 5,
             display: "flex",
             flexDirection: "row",
-            justifyContent: "space-between",
-            padding: 35,
-          }}>
-        <Card>
-          <Card.Content>
-            <View style={{
-              paddingBottom: 50
-            }}>
-              <Text>
-                Möchten du das Fahrzeug "{vehicleInfo.model}" wirklich zurückgeben?
-              </Text>
-              <Text style={{
-                paddingTop: 30
-              }}>
-                Weitere Informationen: {vehicleInfo.additional_information}
-              </Text>
-            </View>
-            <Button
-              mode="contained"
-              onPress={async () => {
-                await removeSupabaseCarEntry();
-                navigation.navigate("Reservations");
-              }}
-            >
-            Fahrzeug zurückgeben
-            </Button>
-          </Card.Content>
-        </Card>
+          }}
+        >
+          <MapView
+            style={{ height: "100%", width: "100%" }}
+            provider={PROVIDER_GOOGLE}
+            ref={(_map) => {
+              if (_map) {
+                map = _map;
+              }
+            }}
+          >
+            {vehicleInfo.latitude && vehicleInfo.longitude && (
+              <Marker
+                key={vehicleInfo.id}
+                coordinate={{
+                  latitude: vehicleInfo.latitude,
+                  longitude: vehicleInfo.longitude,
+                }}
+              />
+            )}
+          </MapView>
+          {locationAddress && locationAddress?.[0] && (
+            <Text
+              variant="titleSmall"
+              style={{ marginLeft: 5, alignSelf: "center" }}
+            >{`${locationAddress[0]?.street} ${locationAddress[0]?.streetNumber}, ${locationAddress[0]?.postalCode} ${locationAddress[0]?.city}`}</Text>
+          )}
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+      </View>
+      <Divider />
+      <View
+        style={{
+          padding: 10,
+        }}
+      >
+        <Text variant="titleLarge">Zusätzliche Informationen</Text>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <Text
+            variant="titleSmall"
+            style={{ marginLeft: 10, alignSelf: "center" }}
+          >
+            {selectedRentable?.additional_information || "N/A"}
+          </Text>
+        </View>
+      </View>
+      <Divider />
+      <View style={{ padding: 10 }}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Text variant="titleMedium">Startdatum: </Text>
+          {isIOS ? (
+            <View/>
+          ) : (
+            <View/>
+          )}
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            marginTop: 5,
+          }}
+        >
+          <Text variant="titleMedium">Startzeit: </Text>
+          {isIOS ? (
+            <View/>
+          ) : (
+            <View/>
+          )}
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            marginTop: 5,
+          }}
+        >
+          <Text variant="titleMedium">Geplantes Enddatum: </Text>
+          {isIOS ? (
+            <View/>
+          ) : (
+            <View/>
+          )}
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            marginTop: 5,
+          }}
+        >
+          <Text variant="titleMedium">Geplante Endzeit: </Text>
+          {isIOS ? (
+            <View/>
+          ) : (
+            <View/>
+          )}
+        </View>
+        {showErorr && (
+          <Text variant="titleMedium">
+            Diese Zeit wurde bereits gebucht. Bitte wählen Sie einen anderen
+            Zeitraum
+          </Text>
+        )}
+        {show && (
+          <View
+            />
+        )}
+      </View>
+    </ScrollView>
+    <View
+      style={{
+        height: "10%",
+        width: "100%",
+        borderColor: MD3Colors.neutral50,
+        borderStyle: "solid",
+        borderTopWidth: 1.5,
+        padding: 10,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      
+      <View style={{ width: "75%" }}>
+        <Text variant="titleSmall">
+          Möchtest du "{vehicleInfo.model}" jetzt zurückgeben?
+        </Text>
+      </View>
+      <Button
+        mode="contained"
+        onPress={async () => {
+          await removeSupabaseCarEntry();
+          navigation.navigate("Reservations");
+        }}
+      >
+        Ja
+      </Button>
+    </View>
+  </SafeAreaView>);
 };
