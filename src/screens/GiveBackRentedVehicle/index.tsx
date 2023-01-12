4de496bf5f5fae0ff41756f2cf90c080497328f6
Joshua Krcmar <1711205@stud.hs-mannheim.de>
2023-01-12 13:36:27 +0100

    Add contact owner functionality to reservations

--- src/screens/GiveBackRentedVehicle/index.tsx
+++ src/screens/GiveBackRentedVehicle/index.tsx
@@ -17,13 +17,17 @@ import {
   MD3Colors,
   Text,
 } from "react-native-paper";
+import moment from "moment";
 
-type Props = NativeStackScreenProps<RootStackParamList, "GiveBackRentedVehicle">;
+type Props = NativeStackScreenProps<
+  RootStackParamList,
+  "GiveBackRentedVehicle"
+>;
 
 export const GiveBackRentedVehicle = ({ route, navigation }: any) => {
-  const [selectedRentable, setSelectedRentable] = useState<Rentable | undefined>(
-    route.params.selectedRentable
-  );
+  const [selectedRentable, setSelectedRentable] = useState<
+    Rentable | undefined
+  >(route.params.selectedRentable);
 
   const isIOS = Platform.OS === "ios";
   const [show, setShow] = useState(false);
@@ -71,231 +75,261 @@ export const GiveBackRentedVehicle = ({ route, navigation }: any) => {
   const currentReservation = route.params.reservation;
   const vehicleInfo = route.params.reservation._rentable;
 
-    const removeSupabaseCarEntry = async () => {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-  
-      const { data, status, error } = await supabase
-        .from('reservations')
-        .delete()
-        .eq('id', currentReservation.id)
-      
-      console.log(error);
-      console.log(data);
-      console.log(status);
-      console.log(currentReservation + " deleted")
-    };
+  const removeSupabaseCarEntry = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
 
-    // const convertUTCToLocalTime = (dateString: any) => {
-    //   let date = new Date(dateString.getUTCDate);
-    //   const milliseconds = Date.UTC(
-    //     date.getFullYear(),
-    //     date.getMonth(),
-    //     date.getDate(),
-    //     date.getHours(),
-    //     date.getMinutes(),
-    //     date.getSeconds(),
-    //   );
-    //   const localTime = new Date(milliseconds);
-    //   localTime.getDate() // local date
-    //   localTime.getHours() // local hour
-    // };
+    const { data, status, error } = await supabase
+      .from("reservations")
+      .delete()
+      .eq("id", currentReservation.id);
 
-    // console.log(convertUTCToLocalTime(currentReservation.created_at))
+    console.log(error);
+    console.log(data);
+    console.log(status);
+    console.log(currentReservation + " deleted");
+  };
 
-    return (<SafeAreaView style={{ flex: 1 }} edges={["bottom", "left", "right"]}>
-    <ScrollView style={{ height: "90%" }}>
-      <View
-        style={{
-          width: "100%",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          padding: 10,
-        }}
-      >
-        <View>
-          <IconButton
-            mode="outlined"
-            icon="car"
-            iconColor={MD3Colors.neutral50}
-            size={25}
-          />
-        </View>
+  // const convertUTCToLocalTime = (dateString: any) => {
+  //   let date = new Date(dateString.getUTCDate);
+  //   const milliseconds = Date.UTC(
+  //     date.getFullYear(),
+  //     date.getMonth(),
+  //     date.getDate(),
+  //     date.getHours(),
+  //     date.getMinutes(),
+  //     date.getSeconds(),
+  //   );
+  //   const localTime = new Date(milliseconds);
+  //   localTime.getDate() // local date
+  //   localTime.getHours() // local hour
+  // };
+
+  // console.log(convertUTCToLocalTime(currentReservation.created_at))
+
+  return (
+    <SafeAreaView style={{ flex: 1 }} edges={["bottom", "left", "right"]}>
+      <ScrollView style={{ height: "90%" }}>
         <View
           style={{
+            width: "100%",
             display: "flex",
+            flexDirection: "row",
             justifyContent: "space-between",
-            marginTop: 10,
-            marginBottom: 10,
+            padding: 10,
           }}
         >
-          <Text variant="titleLarge">{vehicleInfo.model}</Text>
           <View>
-            <Text variant="titleSmall">
-              Kraftstoff: {vehicleInfo.fuel}
-            </Text>
-            <Text variant="titleSmall">
-              Kosten per km: {vehicleInfo.cost_per_km}€
-            </Text>
-            <Text variant="titleSmall">
-              Kosten per minute: {vehicleInfo.cost_per_minute}€
+            <IconButton
+              mode="outlined"
+              icon="car"
+              iconColor={MD3Colors.neutral50}
+              size={25}
+            />
+          </View>
+          <View
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginTop: 10,
+              marginBottom: 10,
+            }}
+          >
+            <Text variant="titleLarge">{vehicleInfo.model}</Text>
+            <View>
+              <Text variant="titleSmall">Kraftstoff: {vehicleInfo.fuel}</Text>
+              <Text variant="titleSmall">
+                Kosten per km: {vehicleInfo.cost_per_km}€
+              </Text>
+              <Text variant="titleSmall">
+                Kosten per minute: {vehicleInfo.cost_per_minute}€
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginTop: 10,
+              marginBottom: 10,
+            }}
+          >
+            <Text variant="titleLarge">Sitze: {vehicleInfo.seat_count}</Text>
+          </View>
+        </View>
+        <Divider />
+        <View
+          style={{
+            padding: 10,
+          }}
+        >
+          <Text variant="titleLarge">Standort</Text>
+          <View
+            style={{
+              height: 125,
+              width: "45%",
+              borderColor: MD3Colors.neutral50,
+              borderStyle: "solid",
+              borderWidth: 1.5,
+              borderRadius: 3,
+              marginTop: 5,
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <MapView
+              style={{ height: "100%", width: "100%" }}
+              provider={PROVIDER_GOOGLE}
+              ref={(_map) => {
+                if (_map) {
+                  map = _map;
+                }
+              }}
+            >
+              {vehicleInfo.latitude && vehicleInfo.longitude && (
+                <Marker
+                  key={vehicleInfo.id}
+                  coordinate={{
+                    latitude: vehicleInfo.latitude,
+                    longitude: vehicleInfo.longitude,
+                  }}
+                />
+              )}
+            </MapView>
+            {locationAddress && locationAddress?.[0] && (
+              <Text
+                variant="titleSmall"
+                style={{ marginLeft: 5, alignSelf: "center" }}
+              >{`${locationAddress[0]?.street} ${locationAddress[0]?.streetNumber}, ${locationAddress[0]?.postalCode} ${locationAddress[0]?.city}`}</Text>
+            )}
+          </View>
+        </View>
+        <Divider />
+        <View
+          style={{
+            padding: 10,
+          }}
+        >
+          <Text variant="titleLarge">Zusätzliche Informationen</Text>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <Text
+              variant="titleSmall"
+              style={{ marginLeft: 10, alignSelf: "center" }}
+            >
+              {vehicleInfo.additional_information || "N/A"}
             </Text>
           </View>
         </View>
-        <View
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            marginTop: 10,
-            marginBottom: 10,
-          }}
-        >
-          <Text variant="titleLarge">
-            Sitze: {vehicleInfo.seat_count}
-          </Text>
-        </View>
-      </View>
-      <Divider />
-      <View
-        style={{
-          padding: 10,
-        }}
-      >
-        <Text variant="titleLarge">Standort</Text>
-        <View
-          style={{
-            height: 125,
-            width: "45%",
-            borderColor: MD3Colors.neutral50,
-            borderStyle: "solid",
-            borderWidth: 1.5,
-            borderRadius: 3,
-            marginTop: 5,
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          <MapView
-            style={{ height: "100%", width: "100%" }}
-            provider={PROVIDER_GOOGLE}
-            ref={(_map) => {
-              if (_map) {
-                map = _map;
-              }
+        <Divider />
+        <View style={{ padding: 10 }}>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
             }}
           >
-            {vehicleInfo.latitude && vehicleInfo.longitude && (
-              <Marker
-                key={vehicleInfo.id}
-                coordinate={{
-                  latitude: vehicleInfo.latitude,
-                  longitude: vehicleInfo.longitude,
-                }}
-              />
-            )}
-          </MapView>
-          {locationAddress && locationAddress?.[0] && (
-            <Text
-              variant="titleSmall"
-              style={{ marginLeft: 5, alignSelf: "center" }}
-            >{`${locationAddress[0]?.street} ${locationAddress[0]?.streetNumber}, ${locationAddress[0]?.postalCode} ${locationAddress[0]?.city}`}</Text>
-          )}
+            <Text variant="titleMedium">
+              {`Gebucht am: ${moment(currentReservation.created_at).format(
+                "DD.MM.YYYY HH:mm"
+              )}`}
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 5,
+            }}
+          >
+            <Text variant="titleMedium">
+              {`Geplanter Start: ${moment(currentReservation.start).format(
+                "DD.MM.YYYY HH:mm"
+              )}`}
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 5,
+            }}
+          >
+            <Text variant="titleMedium">
+              {`Geplantes Ende: ${moment(currentReservation.end).format(
+                "DD.MM.YYYY HH:mm"
+              )}`}
+            </Text>
+          </View>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 5,
+            }}
+          ></View>
         </View>
-      </View>
-      <Divider />
+        <Divider />
+        <View
+          style={{
+            padding: 10,
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <Button
+              mode="contained"
+              onPress={() =>
+                navigation.push("Chat", { chatPartnerId: vehicleInfo?.owner! })
+              }
+            >
+              Fahrzeugbesitzer kontaktieren
+            </Button>
+          </View>
+        </View>
+      </ScrollView>
       <View
         style={{
+          height: "10%",
+          width: "100%",
+          borderColor: MD3Colors.neutral50,
+          borderStyle: "solid",
+          borderTopWidth: 1.5,
           padding: 10,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
         }}
       >
-        <Text variant="titleLarge">Zusätzliche Informationen</Text>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          <Text
-            variant="titleSmall"
-            style={{ marginLeft: 10, alignSelf: "center" }}
-          >
-            {vehicleInfo.additional_information || "N/A"}
+        <View style={{ width: "50%" }}>
+          <Text variant="titleSmall">
+            Möchtest du "{vehicleInfo.model}" jetzt abgeben?
           </Text>
         </View>
+        <Button
+          mode="contained"
+          onPress={async () => {
+            await removeSupabaseCarEntry();
+            navigation.navigate("Reservations");
+          }}
+        >
+          Abgabe bestätigen
+        </Button>
       </View>
-      <Divider />
-      <View style={{ padding: 10 }}>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <Text variant="titleMedium">Gebucht am: {currentReservation.created_at}</Text>
-        </View>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            marginTop: 5,
-          }}
-        >
-          <Text variant="titleMedium">Geplanter Start: {currentReservation.start}</Text>
-        </View>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            marginTop: 5,
-          }}
-        >
-          <Text variant="titleMedium">Geplantes Ende: {currentReservation.end}</Text>
-        </View>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            marginTop: 5,
-          }}
-        >
-        </View>
-      </View>
-    </ScrollView>
-    <View
-      style={{
-        height: "10%",
-        width: "100%",
-        borderColor: MD3Colors.neutral50,
-        borderStyle: "solid",
-        borderTopWidth: 1.5,
-        padding: 10,
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      
-      <View style={{ width: "50%" }}>
-        <Text variant="titleSmall">
-          Möchtest du "{vehicleInfo.model}" jetzt abgeben?
-        </Text>
-      </View>
-      <Button
-        mode="contained"
-        onPress={async () => {
-          await removeSupabaseCarEntry();
-          navigation.navigate("Reservations");
-        }}
-      >
-        Abgabe bestätigen
-      </Button>
-    </View>
-  </SafeAreaView>);
+    </SafeAreaView>
+  );
 };
